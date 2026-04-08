d4ccdf4af736d3a7bfb47aeecaba1c4d0046139d
hisinka <brawlman20@yandex.ru>
2026-04-08 22:00:26 +0300

    проверка бота

--- script.py
+++ script.py
@@ -1,7 +1,14 @@
 from aiogram import Bot, Dispatcher
+from aiogram.types import Message
 
 bot = Bot(token = '8763198603:AAGfXly0jo29YlOgKvEiGesn36CgKCHd9-k')
 dp = Dispatcher()
+
+
+@dp.message()
+async def echo(message: Message):
+    await message.send_copy(chat_id=message.from_user.id)
+
 
 async def main():
     await dp.start_polling(bot)
