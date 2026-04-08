e3aa371fe136eb6f0a870aa4ab878c07916275d0
hisinka <brawlman20@yandex.ru>
2026-04-08 21:51:13 +0300

    update

--- script.py
+++ script.py
@@ -1,2 +1,15 @@
-print("hello")
-print("hello")
+from aiogram import Bot, Dispatcher
+
+bot = Bot(token = '8763198603:AAGfXly0jo29YlOgKvEiGesn36CgKCHd9-k')
+dp = Dispatcher()
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    try:
+        import asyncio
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
