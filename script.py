fa48d4a9aab824272a5643a863a2117b058fbea1
hisinka <brawlman20@yandex.ru>
2026-04-08 22:31:19 +0300

    update3

--- script.py
+++ script.py
@@ -1,13 +1,24 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
+from aiogram.filters.command import CommandStart, Command
 
 bot = Bot(token = '8763198603:AAGfXly0jo29YlOgKvEiGesn36CgKCHd9-k')
 dp = Dispatcher()
 
 
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer('Добро пожаловать в бот')
+
+
+@dp.message(command('help'))
+async def help(message: Message):
+    await message.answer('Вы написали команду /help')
+
+
 @dp.message()
 async def echo(message: Message):
-    await message.send_copy(chat_id=message.from_user.id)
+    await message.answer('неизвестная комманда')
 
 
 async def main():
